0b1cbd25318b3633e0de0375bf04a91b260799e6
Prisha1801 <prince.anantkamalstudios@gmail.com>
2026-09-04 10:11:00 +0530

    bug 13

--- src/components/CandidateApplicationForm/CandidateApplicationReadonly.jsx
+++ src/components/CandidateApplicationForm/CandidateApplicationReadonly.jsx
@@ -67,6 +67,11 @@ const FIELD_SECTIONS = [
       "otherBreach",
       "otherBreachDetails",
       "refusedVisa",
+      "refusedVisaReason",
+      "refusedVisaDate",
+      "refusedVisaCountry",
+      "refusedVisaType",
+      "refusedVisaReference",
       "refusedVisaDetails",
       "refusedEntry",
       "refusedEntryDetails",
@@ -157,6 +162,11 @@ function buildSections(form, customFieldDefinitions = []) {
           breachDetails: "breach",
           falseInfoDetails: "falseInfo",
           otherBreachDetails: "otherBreach",
+          refusedVisaReason: "refusedVisa",
+          refusedVisaDate: "refusedVisa",
+          refusedVisaCountry: "refusedVisa",
+          refusedVisaType: "refusedVisa",
+          refusedVisaReference: "refusedVisa",
           refusedVisaDetails: "refusedVisa",
           refusedEntryDetails: "refusedEntry",
           refusedPermissionDetails: "refusedPermission",
@@ -168,6 +178,9 @@ function buildSections(form, customFieldDefinitions = []) {
           sponsoredDetails: "sponsored",
         };
         if (PARENT_MAP[key] && form[PARENT_MAP[key]] !== "Yes") {
+          return false;
+        }
+        if (key === "refusedVisaDetails" && form.refusedVisaReason) {
           return false;
         }
         return true;
